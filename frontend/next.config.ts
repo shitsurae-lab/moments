99829a6f16661dc696a26dff98f1509010bd2f8c
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-04-02 13:30:58 +0900

    chore: Docker環境用にTurbopackのルートパスを修正

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
-        pathname: '/**', //すべてのパスを許可
+        pathname: '/**',
       },
     ],
+  },
+  turbopack: {
+    root: '/app',
   },
 };
 
