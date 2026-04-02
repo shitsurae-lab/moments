ca52c3278f3fca74bb4bc0d9d1c794dfbd875b85
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-04-02 17:27:32 +0900

    feat: fillableにtitle・user_name・user_avatar_urlを追加

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
-    protected $fillable = ['image_path', 'caption', 'tags'];
+    //保存可能な属性を指定する
+    protected $fillable = ['image_path', 'title', 'caption', 'tags', 'user_name', 'user_avatar_url'];
 }
