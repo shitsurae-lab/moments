276a779671c01a536091569513f3b50e1bb48dd0
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-04-02 17:28:03 +0900

    feat: 本番DBリセットのためmigrate:freshに一時変更

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -3,11 +3,10 @@ set -e
 
 php-fpm &
 
-# php-fpm の起動を少し待つ
 sleep 2
 
 php artisan config:cache
-php artisan migrate --force
+php artisan migrate:fresh --force
 php artisan storage:link
 
 nginx -g "daemon off;"
