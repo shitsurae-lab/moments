1ffea8051e18cddcb9af659f367d43fd49d273b9
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-04-14 11:46:15 +0900

    feat: 投稿はUserに従属するという関係を定義

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     //保存可能な属性を指定する
-    protected $fillable = ['image_path', 'title', 'caption', 'tags', 'user_name', 'user_avatar_url'];
+    protected $fillable = ['user_id', 'image_path', 'title', 'caption', 'tags', 'user_name', 'user_avatar_url'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
