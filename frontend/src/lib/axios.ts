581225b5f2b72543a349640b14cd66343c8f1dfd
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-04-18 08:40:06 +0900

    fix: baseURLを空文字に変更してrewritesに委譲

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3080',
+  baseURL: '',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     Accept: 'application/json',
